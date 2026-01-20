9ad6d6845b973eccb1b3bcef7d2640740ae85cd9
F. Alazzarotto <falazzarotto@gmail.com>
2026-01-20 13:55:48 -0300

    Arquivos adicionados com sucesso

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Landing Page Carregada");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
